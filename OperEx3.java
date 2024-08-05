65bbcd5192ba458404ba537ed2f95b96cdcec3cf
ays92 <162099828+ays92@users.noreply.github.com>
2024-08-05 17:19:22 +0900

    Add files via upload

---
+++ OperEx3.java
@@ -0,0 +1,10 @@
+package operator_연산자.ex;
+
+public class OperEx3 {
+    public static void main(String[] args) {
+        int score = 70;
+        boolean result = score >=80 && score <= 100;
+
+        System.out.println(result);
+    }
+}
